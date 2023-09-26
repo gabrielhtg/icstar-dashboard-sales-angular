76f3c701145b0c8ee5915bca419d0106c4b81db1
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2023-09-26 23:19:45 +0700

    kembali ke tailwind

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, NgModule } from '@angular/core';
 
 @Component({
@@ -5,4 +6,19 @@ import { Component, NgModule } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public getJsonValue: any;
+  public postJsonValue: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getMethod();
+  }
+
+  public getMethod() {
+    this.http.get('http://localhost:8080/api/login').subscribe((data) => {
+      console.log(data);
+    });
+  }
+}
